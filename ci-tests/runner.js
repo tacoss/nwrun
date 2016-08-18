e922146a8d59c17da47034c204a64acf21c6c051
Juan Mendivil <juan@mendivil.mx>
2016-08-18 11:17:03 -0500

    fix saucelabs config

--- ci-tests/runner.js
+++ ci-tests/runner.js
@@ -11,12 +11,10 @@ nwrun({
   test_settings: {
     saucelabs: {
       silent: true,
-      selenium_host: 'localhost',
-      selenium_port: 4445,
-      desiredCapabilities: {
-        username: process.env.SAUCE_USERNAME,
-        accessKey: process.env.SAUCE_ACCESS_KEY
-      }
+      selenium_host: 'ondemand.saucelabs.com',
+      selenium_port: 80,
+      username: '${SAUCE_USERNAME}',
+      access_key: '${SAUCE_ACCESS_KEY}',
     }
   }
 }, function(done) {
